929dd1d47c6ff162a57e9710c1ae6bdd955c5776
Shiv Tyagi <shivtyagi3015@gmail.com>
2024-06-07 13:52:49 +0530

    models: add signature request model

--- models.py
+++ models.py
@@ -9,6 +9,10 @@ class Payload(BaseModel):
     data: Union[str, None] = None
     parties: Parties
 
+class SignatureRequest(BaseModel):
+    payload:Payload
+    auth_token: str
+
 class SignedPayload(BaseModel):
     payload_to_verify: Payload
     signature: str
