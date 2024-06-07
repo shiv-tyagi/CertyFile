493d6e521810b47b1b5283da140483180f2f8550
Shiv Tyagi <shivtyagi3015@gmail.com>
2024-06-07 13:39:09 +0530

    models: add Parties model and use it as required

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
 from typing import Union
 
+class Parties(BaseModel):
+    one: str
+    two: Union[str, None] = None
 
 class PayloadToSign(BaseModel):
     data_to_sign: Union[str, None] = None
-    first_party: str
-    second_party: Union[str, None] = None
-
+    parties: Parties
 
 class SignedPayload(BaseModel):
     data_to_verify: PayloadToSign
