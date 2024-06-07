23cbfaa3ffbb010a69ba39f22d80f90c8bd15242
Shiv Tyagi <shivtyagi3015@gmail.com>
2024-06-07 22:17:52 +0530

    static: changes to support demo auth token workflow

--- static/js/index.js
+++ static/js/index.js
@@ -5,29 +5,56 @@ document.getElementById("signForm").addEventListener("submit", function(event) {
     const partyOne = document.getElementById('partyOneEmail').value;
     const partyTwo = document.getElementById('partyTwoEmail').value;
 
-    const data = {
-        data_to_sign: plainText,
-        first_party: partyOne,
-        second_party: partyTwo
+    const parties = {
+        one: partyOne,
+        two: partyTwo
+    }
+
+    const token_request = {
+        parties: parties,
     };
 
-    fetch('/sign', {
-        method: 'POST',
+    fetch("/auth_token", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(token_request)
     })
     .then(response => {
         if (response.ok) {
             return response.text();
         } else {
-            throw new Error(`${response.status} ${response.statusText}`);
+            throw new Error(`/auth_token: ${response.status} ${response.statusText}`);
         }
     })
-    .then(responseText => {
-        document.getElementById('outputTextArea').value = responseText.replace(/\"/g, "");
-        var output_modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('signatureOutputModal'));
+    .then(token => {
+        const signature_request = {
+            payload: {
+                parties: parties,
+                data: plainText
+            },
+            auth_token: JSON.parse(token)
+        }
+
+        return fetch("/sign", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(signature_request)
+        });
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw new Error(`/sign: ${response.status} ${response.statusText}`);
+        }
+    })
+    .then(signature => {
+        document.getElementById('outputTextArea').value = JSON.parse(signature);
+        var output_modal = bootstrap.Modal.getOrCreateInstance(document.getElementById("signatureOutputModal"));
         output_modal.show();
     })
     .catch(error => {
@@ -37,10 +64,7 @@ document.getElementById("signForm").addEventListener("submit", function(event) {
 
 document.getElementById("copySignatureButton").addEventListener("click", function(event) {
     var text_element = document.getElementById("outputTextArea");
-
     text_element.select();
-
     navigator.clipboard.writeText(text_element.value);
-
     alert("Copied the signature hex to clipboard");
 });
